a1d3eb56f406b393a7e3ed4b3c7fb87172f71799
svetttllana <szgirovskaya@mail.ru>
2022-10-10 19:35:02 +0300

    test push

--- lib/src/main/java/com/example/lib/Lesson51.java
+++ lib/src/main/java/com/example/lib/Lesson51.java
@@ -2,31 +2,32 @@ package com.example.lib;
 
 public class Lesson51 {
     public static void main(String[] args) {
-     //   friend friend = new friend("shasha");
-        //friend.age =18;
-        //friend.name ="Shasha";
-       // friend2.born();
-      //  friend.born();
+        friend friend = new friend("Shasha", 18, "20.02.2001");
+ //    friend friend2 = new friend("Dima",28);
+        friend.age =18;
+     friend.name ="Shasha";
+   //   friend2.born();
+       friend.born();
     }
 }
 
 class friend {
 
-    String name;
-    int age;
-    String birthdate;
+    public int age;
+    public String name;
+       public String birthdate;
 
-    //public friend(String name2, int age2, String birthdate2) {
-      //  this.name = name2;
-        //this.age = age2;
-        //this.birthdate = birthdate2;
+   //public friend(String name2, int age2, String birthdate2) {
+     // this.name = name2;
+    //this.age = age2;
+    //this.birthdate = birthdate2;
 
-   // }
+   //}
 
     public friend(String name2, int age2, String birthdate2){
-    this.name =name2;
-    this.age =age2;
-    this.birthdate = birthdate2;
+        this.name =name2;
+        this.age =age2;
+        this.birthdate = birthdate2;
 }
     void born(){
         String congratulations= " ";
@@ -34,7 +35,7 @@ class friend {
             congratulations = "no birthday";
         }else{
 
-            congratulations = name + name + "hes berthday on:" + birthdate + " " + "he is" + age + " years old";
+            congratulations = name + "hes berthday on:" + birthdate + " " + "he is" + age + " years old";
         }
         System.out.println(congratulations);
     }
