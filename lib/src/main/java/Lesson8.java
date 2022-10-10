52320e815b5191db50c50ad3bc4c81c62ea8207b
svetttllana <szgirovskaya@mail.ru>
2022-10-10 22:00:40 +0300

    Lesson8; work with isklyshenizmi  + Shop

--- lib/src/main/java/Lesson8.java
+++ lib/src/main/java/Lesson8.java
@@ -1,5 +1,23 @@
 public class Lesson8 {
     public static void main(String[] args) {
-System.out.println("Hello world");
+        try {
+            getWrongValue(null);
+        }catch (ArithmeticException  | NullPointerException |IndexOutOfBoundsException e) {
+            System.out.println("oshibka");
+        }finally{
+            System.out.println(" do if success or exeption throw");
+//        } catch (NullPointerException e) {
+//            System.out.println("caught NullPointExeption");
+
+//        } catch (IndexOutOfBoundsException e) {
+//            System.out.println("caught exeption");
+
+//        } catch (RuntimeException e) {
+//            System.out.println("caught RunTimeExeption");
+        }
     }
-}
+        static void getWrongValue (Integer value){
+            System.out.println(value.toString());
+        }
+    }
+
